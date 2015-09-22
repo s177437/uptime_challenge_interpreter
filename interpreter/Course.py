6994602ed480d846f44c86afa3926eaaf445b491
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-09-22 21:24:53 +0200

    new content

--- interpreter/Course.py
+++ interpreter/Course.py
@@ -12,6 +12,10 @@ class Course ():
     def get_groups(self):
         return self.groups
     def setGroupList(self,group):
-        self.groups.append(group)
+        for i in self.groups :
+            if group.get_groupname() == i.get_groupname() :
+                print "The group already exists in the accountobject"
+            else :
+                self.groups.append(group)
         
         
