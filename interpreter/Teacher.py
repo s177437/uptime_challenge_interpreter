5e177eba10415effbb077a61b9234f1bb5d88451
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-09-27 20:30:34 +0200

    content for semester

--- interpreter/Teacher.py
+++ interpreter/Teacher.py
@@ -1,16 +1,22 @@
-class Teacher ():
+class Teacher():
     name = ""
     course = ""
+    semester = ""
+
     def get_name(self):
         return self.__name
 
+    def set_semester(self, s):
+        self.semester = s
+
+    def get_semester(self):
+        return self.semester
 
     def set_name(self, value):
         self.__name = value
 
     def get_course(self):
         return self.__course
-
 
     def set_course(self, value):
         self.__course = value
